ca5ae9fc5fc29d4a9e2f96c2f162b0210c365dd0
ss0g <55930267+ss0g@users.noreply.github.com>
2022-10-25 09:08:41 -0700

    assignment 13 and assignment 14

---
+++ assignment14/assignment14.R
@@ -0,0 +1,28 @@
+setwd("C:/Users/tntje/work/school/applied-stats/assignment14")
+
+library(ggplot2)
+
+data("mtcars")
+
+bp1 <- ggplot(data = mtcars, aes(x = am, y = qsec)) +
+  geom_boxplot(aes(group = am))
+bp1
+
+bc1 <- ggplot(data = mtcars, aes(x = factor(carb))) +
+  geom_bar()
+bc1
+
+bc2 <- ggplot(data = mtcars, aes(x = factor(gear), fill = factor(cyl))) +
+  geom_bar(position = "stack")
+bc2
+
+sp1 <- ggplot(data = mtcars, aes(x = wt, y = mpg)) +
+  geom_point()
+sp1
+
+lm_sp2 <- lm(mtcars$qsec~mtcars$hp/mtcars$wt)
+
+
+sp2 <- ggplot(data = mtcars, aes(x = hp / wt)) +
+  geom_histogram(binwidth = 10)
+sp2
